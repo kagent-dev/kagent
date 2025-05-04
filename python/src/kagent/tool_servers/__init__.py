12f924e9a690eda5fee35bc5251a5bc0b0930124
Dmytro Rashko <dmitriy.rashko@amdocs.com>
2025-05-04 11:08:51 +0200

    cleanup

--- python/src/kagent/tool_servers/__init__.py
+++ python/src/kagent/tool_servers/__init__.py
@@ -2,11 +2,8 @@ from ._ssemcptoolserver import SseMcpToolServer, SseMcpToolServerConfig
 from ._stdiomcptoolserver import StdioMcpToolServer, StdioMcpToolServerConfig
 
 __all__ = [
-    "ToolServer",
     "SseMcpToolServer",
     "SseMcpToolServerConfig",
     "StdioMcpToolServer",
     "StdioMcpToolServerConfig",
 ]
-
-from ._tool_server import ToolServer
