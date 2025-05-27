c94f8cef98b6a4c4ced18c40793da46631f4e12e
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2025-05-27 08:06:33 +0530

    fix: update paths for kubectl, istioctl, helm and add kubectl-argo-rollouts

--- python/src/kagent/tools/common/_binary_paths.py
+++ python/src/kagent/tools/common/_binary_paths.py
@@ -5,16 +5,21 @@ class BinaryPathsConfig(BaseModel):
     """Configuration for binary paths used by various tools."""
     
     kubectl_path: Optional[str] = Field(
-        default="kubectl",
-        description="Path to the kubectl binary. If not specified, will use 'kubectl' from PATH."
+        default="/usr/local/bin/kubectl",
+        description="Path to the kubectl binary. If not specified, will use '/usr/local/bin/kubectl'."
     )
     
     istioctl_path: Optional[str] = Field(
-        default="istioctl",
-        description="Path to the istioctl binary. If not specified, will use 'istioctl' from PATH."
+        default="/usr/local/bin/istioctl",
+        description="Path to the istioctl binary. If not specified, will use '/usr/local/bin/istioctl'."
     )
     
     helm_path: Optional[str] = Field(
-        default="helm",
-        description="Path to the helm binary. If not specified, will use 'helm' from PATH."
+        default="/usr/local/bin/helm",
+        description="Path to the helm binary. If not specified, will use '/usr/local/bin/helm'."
+    )
+
+    kubectl_argo_rollouts_path: Optional[str] = Field(
+        default="/usr/local/bin/kubectl-argo-rollouts",
+        description="Path to the kubectl-argo-rollouts binary. If not specified, will use '/usr/local/bin/kubectl-argo-rollouts'."
     ) 
