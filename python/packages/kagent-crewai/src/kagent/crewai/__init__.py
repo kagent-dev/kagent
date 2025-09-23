a0a8d3d75dbd1b752ab6ae04d68bf8d8ee2cd86b
Jet Chiang <jetjiang.ez@gmail.com>
2025-09-22 20:38:20 -0400

    remove init in crewai package
    Signed-off-by: Jet Chiang <jetjiang.ez@gmail.com>

--- python/packages/kagent-crewai/src/kagent/crewai/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._a2a import KAgentApp
-from ._executor import CrewAIAgentExecutor
-
-__all__ = ["KAgentApp", "CrewAIAgentExecutor", "CrewConfig", "AgentConfig", "TaskConfig"]
-__version__ = "0.1.0"
