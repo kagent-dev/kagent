fd9eae9b3bbd6cf52c0890ca0f5cdc220464c29c
insomniac2904 <adarshkumar7478@gmail.com>
2026-01-25 21:33:18 +0530

    fixing py formatting
    Signed-off-by: insomniac2904 <adarshkumar7478@gmail.com>

--- python/packages/kagent-adk/src/kagent/adk/types.py
+++ python/packages/kagent-adk/src/kagent/adk/types.py
@@ -241,7 +241,9 @@ class AgentConfig(BaseModel):
         elif self.model.type == "gemini_anthropic":
             model = ClaudeLLM(model=self.model.model)
         elif self.model.type == "ollama":
-            model = LiteLlm(model=f"ollama_chat/{self.model.model}", extra_headers=extra_headers, **(self.model.options or {}))
+            model = LiteLlm(
+                model=f"ollama_chat/{self.model.model}", extra_headers=extra_headers, **(self.model.options or {})
+            )
         elif self.model.type == "azure_openai":
             model = OpenAIAzure(
                 model=self.model.model,
