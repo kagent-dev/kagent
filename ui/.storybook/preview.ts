f6938bc437e5f8368365f7e8d81068a36af6322c
Dobes Vandermeer <dobes.vandermeer@newsela.com>
2026-02-22 17:34:12 -0800

    Add storybook
    When working on UI elements it is a lot easier to iterate and test in storybook where you can put up the interesting cases as stories and browse through them instead of having construct scenarios in a live running app.

---
+++ ui/.storybook/preview.ts
@@ -0,0 +1,24 @@
+import type { Preview } from '@storybook/nextjs-vite'
+import '../src/app/globals.css'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+    a11y: {
+      test: 'todo'
+    }
+  },
+  decorators: [
+    (Story) => {
+      document.documentElement.classList.add('dark');
+      return Story();
+    },
+  ],
+};
+
+export default preview;
