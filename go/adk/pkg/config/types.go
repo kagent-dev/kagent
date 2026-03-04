84c1dbe87b3b2c6dfbac0f0567b52459e277e512
Eitan Yarmush <eitan.yarmush@solo.io>
2026-03-04 22:35:08 +0000

    PR comments
    Signed-off-by: Eitan Yarmush <eitan.yarmush@solo.io>

--- go/adk/pkg/config/types.go
+++
@@ -1,4 +0,0 @@
-package config
-
-// This file previously contained type re-exports from go/api/adk for backward compatibility.
-// All types are now imported directly from github.com/kagent-dev/kagent/go/api/adk.
