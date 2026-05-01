b3bbcd3a1d13fd9095ac47968fe965250b47cb6e
JM Huibonhoa <jm.huibonhoa@solo.io>
2026-05-01 16:50:47 -0400

    fix unintended paste
    Signed-off-by: JM Huibonhoa <jm.huibonhoa@solo.io>

--- go/api/database/client.go
+++ go/api/database/client.go
@@ -39,7 +39,7 @@ type Client interface {
 
 	// Get methods
 	GetSession(ctx context.Context, sessionID string, userID string) (*Session, error)
-	go/core/internal/a2a/a2a_registrar.go	GetSessionByID(ctx context.Context, sessionID string) (*Session, error)
+	GetSessionByID(ctx context.Context, sessionID string) (*Session, error)
 	GetAgent(ctx context.Context, name string) (*Agent, error)
 	GetTask(ctx context.Context, id string) (*protocol.Task, error)
 	GetTool(ctx context.Context, name string) (*Tool, error)
