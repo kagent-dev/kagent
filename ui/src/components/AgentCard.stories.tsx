e8cf6dfb56711687485fd763497e6526619e31f4
Eitan Yarmush <eitan.yarmush@solo.io>
2026-08-11 14:44:04 +0000

    fix: provide tooltip context in agent stories
    Signed-off-by: Eitan Yarmush <eitan.yarmush@solo.io>

--- ui/src/components/AgentCard.stories.tsx
+++ ui/src/components/AgentCard.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import { AgentCard } from "./AgentCard";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import type { AgentResponse } from "@/types";
 
 const meta = {
@@ -8,6 +9,7 @@ const meta = {
   parameters: {
     layout: "centered",
   },
+  decorators: [(Story) => <TooltipProvider><Story /></TooltipProvider>],
 } satisfies Meta<typeof AgentCard>;
 
 export default meta;
