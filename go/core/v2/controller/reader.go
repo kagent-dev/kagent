f01815add739c30ebc31a180635536a7a03fe4e8
ekam-walia <164983405+ekam-walia@users.noreply.github.com>
2026-08-27 08:41:13 +0530

    Indentation Fix
    Signed-off-by: ekam-walia <164983405+ekam-walia@users.noreply.github.com>

--- go/core/v2/controller/reader.go
+++ go/core/v2/controller/reader.go
@@ -71,10 +71,10 @@ func (r collectionReader) Get(_ context.Context, key types.NamespacedName, objec
 }
 
 func (r collectionReader) fetchObject[T controllers.ComparableObject](collection krt.Collection[T], key types.NamespacedName, resource schema.GroupResource) (T, error) {
-    object := krt.FetchOne(r.ctx, collection, krt.FilterNamespace(key.Namespace), krt.FilterName(key.Name))
-    if object == nil {
-        var zero T
-        return zero, apierrors.NewNotFound(resource, key.Name)
-    }
-    return *object, nil
+	object := krt.FetchOne(r.ctx, collection, krt.FilterNamespace(key.Namespace), krt.FilterName(key.Name))
+	if object == nil {
+		var zero T
+		return zero, apierrors.NewNotFound(resource, key.Name)
+	}
+	return *object, nil
 }
