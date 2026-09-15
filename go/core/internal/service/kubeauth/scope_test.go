d1daa0a3fb436996d126f8b7ac835c06f19827ca
Cody Hartsook <cody.hartsook@solo.io>
2026-09-15 13:20:53 -0700

    test: remove legacy resource name assertion
    Signed-off-by: Cody Hartsook <cody.hartsook@solo.io>

--- go/core/internal/service/kubeauth/scope_test.go
+++ go/core/internal/service/kubeauth/scope_test.go
@@ -118,7 +118,7 @@ func TestCompileScopeRejectsInvalidScopes(t *testing.T) {
 func TestResourceUsesObjectMetadata(t *testing.T) {
 	object := &metav1.PartialObjectMetadata{ObjectMeta: metav1.ObjectMeta{Namespace: "team-a", Name: "agent-a"}}
 	resource := kubeauth.Resource("Harness", object)
-	if resource.Type != "Harness" || resource.Name != "" {
+	if resource.Type != "Harness" {
 		t.Fatalf("Resource() = %+v", resource)
 	}
 	if got := resource.Attributes[apiauthorization.AttributeNamespace]; got != "team-a" {
